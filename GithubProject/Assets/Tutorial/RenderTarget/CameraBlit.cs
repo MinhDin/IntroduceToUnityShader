91d06b036c143fc8b1023a29ed1291d9a325e068
Dinh Ngoc Minh <minh.dinhngoc@gameloft.com>
2018-02-13 11:53:31 +0700

    +Add render target

---
+++ GithubProject/Assets/Tutorial/RenderTarget/CameraBlit.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraBlit : MonoBehaviour
+{
+    private bool hasInited = false;
+    private Camera cam;
+
+    private bool isEnabled;
+
+    private Vector2 startPosition;
+    public Material TransitionMaterial;
+
+	private RenderTexture renderTexture;
+
+	private int mode;
+
+	public SpriteRenderer[] Sprs;
+	public Color ActiveColor;
+	public Color DeactiveColor;
+	public Transform Hero;
+
+	int colorMode = 0;
+
+    public void Awake()
+    {		
+		colorMode = 0;
+        Init();
+    }
+
+    public void Init()
+    {
+        if (hasInited) return;
+
+        hasInited = true;
+		cam = GetComponent<Camera>();
+
+        isEnabled = false;
+    }
+	
+    public void Deactivate()
+    {
+        isEnabled = false;
+
+		RenderTexture.ReleaseTemporary (renderTexture);
+		renderTexture = null;
+		mode = 0;
+		if (mode == 1)
+		{
+			TransitionMaterial.SetFloat ("_NormalizedTime", 0.0f);
+			TransitionMaterial.SetVector ("_DisplayInfo", Vector4.zero);
+		}
+
+	}
+
+	public void ActivateFakeScreen(RenderTexture renderTexture)
+	{
+		isEnabled = true;
+		mode = 0;
+
+		this.renderTexture = renderTexture;
+	}
+
+	IEnumerator Transition()
+	{
+		float t = 0;
+
+		while(t < 1)
+		{
+			t += Time.deltaTime;
+			SetNormalizedTime(t);
+			yield return null;
+		}
+
+		Deactivate();
+	}
+
+	public void ActivateEnvTransition(Vector2 startPosition, RenderTexture renderTexture)
+    {
+        isEnabled = true;
+		mode = 1;
+
+        this.startPosition = startPosition;
+		this.renderTexture = renderTexture;
+
+		Color col;
+		if(colorMode == 0)
+		{
+			col = ActiveColor;
+			colorMode = 1;
+		}
+		else
+		{
+			col = DeactiveColor;
+			colorMode = 0;
+		}
+
+		for(int i = 0; i < Sprs.Length; ++i)
+		{
+			Sprs[i].color = col;
+		}
+		
+		StartCoroutine(Transition());
+    }
+
+    public void SetNormalizedTime(float normalizedTime)
+    {
+		TransitionMaterial.SetFloat("_NormalizedTime", normalizedTime);
+    }
+
+    public void Update()
+    {
+
+        if (!isEnabled)
+		{
+			if(Input.GetKeyDown(KeyCode.B))
+			{
+				ActivateEnvTransition(Hero.position, GetScreenShot());
+			}
+			return;
+		}
+
+		
+
+		if (mode == 1)
+		{
+			float halfScreenH = cam.orthographicSize;
+			float halfScreenW = halfScreenH * cam.aspect;
+
+			Vector2 camPos = cam.transform.position;
+			Vector2 offset = startPosition - camPos;
+
+			TransitionMaterial.SetVector ("_DisplayInfo", new Vector4 (halfScreenW, halfScreenH, offset.x, offset.y));
+		}
+    }
+
+	private void OnPostRender()
+	{
+		if (!isEnabled) return;
+
+		if (mode == 0) Graphics.Blit (renderTexture, (RenderTexture)null);
+		else Graphics.Blit(renderTexture, null, TransitionMaterial);
+	}
+
+	public RenderTexture GetScreenShot()
+    {
+		int w = Screen.width;
+		int h = Screen.height;
+
+		RenderTexture renderTarget = RenderTexture.GetTemporary(w, h, 0, RenderTextureFormat.ARGB32);        
+		cam.targetTexture = renderTarget;
+	
+		cam.Render();
+
+		RenderTexture.active = renderTarget;
+
+        Texture2D tex = new Texture2D(w, h, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, w, h), 0, 0);
+        tex.Apply();
+
+		RenderTexture.active = null;
+		cam.targetTexture = null;
+
+		return renderTarget;
+    }
+}
---
+++ GithubProject/Assets/Tutorial/RenderTarget/EnvironmentOverlay.shader
@@ -0,0 +1,89 @@
+﻿Shader "Custom/EnvironmentOverlay"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_TransitionTex("Transition Texture", 2D) = "white" {}
+		_GlowTex("Glow Texture", 2D) = "black" {}
+
+		_NormalizedTime("Normalized Time", Range (0.0, 1.0)) = 0.0
+		_DisplayInfo("Display Info", Vector) = (8.5, 13.6, 0.0, 0.0)
+	}
+
+	SubShader
+	{
+		Tags
+		{ 
+			"Queue"="Transparent" 
+			"IgnoreProjector"="True" 
+			"RenderType"="Transparent" 
+			"PreviewType"="Plane"
+		}
+
+		Cull Off
+		ZWrite Off
+		ZTest Always
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			sampler2D _TransitionTex;
+			sampler2D _GlowTex;
+
+			float _NormalizedTime;
+
+			float4 _DisplayInfo;
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				float2 tc = i.uv;
+
+				float2 halfSize = _DisplayInfo.xy;
+				float2 center = _DisplayInfo.zw;
+
+				float2 p = (-1.0 + 2.0 * i.uv) * halfSize - center;
+
+				float len = length(p);
+				float2 uv = i.uv + (p / len) * cos(len * 1.0 - _NormalizedTime * 6.0) * 0.003 * halfSize * min(_NormalizedTime * 7.5, 1.0);
+
+				float3 col = tex2D(_MainTex, uv).xyz;
+				
+				float transit = tex2D(_TransitionTex, uv).x;
+				
+				float alpha = clamp(len * 0.5 - _NormalizedTime * 25.0 + transit * 2.5, 0.0, 1.0);
+
+				col += alpha * tex2D(_GlowTex, fixed2(alpha, 0.0)).xyz;
+				return fixed4(col, alpha);
+			}
+
+			ENDCG
+		}
+	}
+}
